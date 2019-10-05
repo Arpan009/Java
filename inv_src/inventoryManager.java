ef646ef3c51fe7e8495ac4f52084e39e72077718
Arpan Sharma <arpansharma@my.unt.edu>
2019-10-05 13:38:12 -0500

    Create inventoryManager.java

---
+++ inv_src/inventoryManager.java
@@ -0,0 +1,14 @@
+protected class inventoryManager
+{
+    private int count;
+    public void getCount()
+    {
+        System.out.print(this.count);
+
+    }
+    public int setCount(int count)
+    {
+        this.count = count;
+        
+    }
+}
